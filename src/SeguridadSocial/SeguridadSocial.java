3fc68c5b1be75a74edc95d28a58dc176cbbf117c
SheilaRos <sheila.ros.cheme@gmail.com>
2016-09-29 21:06:21 +0200

    HashMap

--- src/SeguridadSocial/SeguridadSocial.java
+++ src/SeguridadSocial/SeguridadSocial.java
@@ -1,80 +1,55 @@
 package SeguridadSocial;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SeguridadSocial{
-    private List<Persona> personasList;
-    public SeguridadSocial() {
-        personasList = new ArrayList<>();
-    }
+
+    private Map <String, Persona> personaMapDNI= new HashMap();
+    private Map <String, Persona> personaMapSS= new HashMap();
+   /* El constructor no es necesario
+   public SeguridadSocial() {
+        personaMapDNI= new HashMap();
+        personaMapSS= new HashMap();
+    }*/
 
     // Debes comprobar que no se introduzcan dos personas con el mismo DNI/Número Seguridad Social
     public void altaPersona(Persona persona) {
-        boolean existe=false;
-        for (int i=0; i<personasList.size();i++){
-            if(personasList.get(i).getDNI().equals(persona.getDNI()) || personasList.get(i).getNumSeguridadSocial().equals(persona.getNumSeguridadSocial())){
-                existe=true;
-            }
-        }
-        if(existe==false){
-            personasList.add(persona);
-        }
+       if(!personaMapDNI.containsKey(persona.getDNI()) && !personaMapSS.containsKey(persona.getNumSeguridadSocial())){
+            personaMapDNI.put(persona.getDNI(), persona);
+            personaMapSS.put(persona.getNumSeguridadSocial(), persona);
+       }
     }
-
     public void bajaPersona(String dni) {
-        personasList.removeIf(persona -> persona.getDNI().equals(dni));
+        personaMapSS.remove(personaMapDNI.get(dni).getNumSeguridadSocial());
+        personaMapDNI.remove(dni);
     }
-
     public Persona obtenerPersonaPorDNI(String dni) {
-        int posicion=0;
-        for (int i=0; i<personasList.size();i++){
-            if(personasList.get(i).getDNI().equals(dni)){
-                posicion=i;
-                break;
-            }
-        }
-        return personasList.get(posicion);
+        return personaMapDNI.get(dni);
     }
-
     public Persona obtenerPersonaPorNumSS(String numSS) {
-        int posicion=0;
-        for (int i=0; i<personasList.size();i++){
-            if(personasList.get(i).getNumSeguridadSocial().equals(numSS)){
-                posicion=i;
-                break;
-            }
-        }
-        return personasList.get(posicion);
+        return personaMapSS.get(numSS);
     }
-
     public List<Persona> obtenerPersonasRangoSalarial(double min, double max){
-        List<Persona> listapersonas = new ArrayList<>();
-        for (int i=0; i<personasList.size(); i++){
-            if(personasList.get(i).getSalario()>=min && personasList.get(i).getSalario()<=max){
-                listapersonas.add(personasList.get(i));
+        //Java 8:
+       return personaMapSS.values().stream().filter(persona -> persona.getSalario()>=min && persona.getSalario()<=max).collect(Collectors.toList());
+       /*Manera tradicional:
+         List<Persona> aux=new ArrayList<>();
+            for(Persona persona:personaMapDNI.values()){
+                if(persona.getSalario()>=min && persona.getSalario()<=max){
+                    aux.add(persona);
+                }
             }
-        }
-        return listapersonas;
+            return aux;
+       */
     }
-
     public List<Persona> obtenerPersonasMayoresQue(int edad){
-        List<Persona> listapersonas = new ArrayList<>();
-        for (int i=0; i<personasList.size(); i++){
-            if(personasList.get(i).getEdad()>edad){
-                listapersonas.add(personasList.get(i));
-            }
-        }
-        return listapersonas;
+        return personaMapDNI.values().stream().filter(persona -> persona.getEdad()>edad).collect(Collectors.toList());
     }
-
     public List<Persona> obtenerTodas(){
-        return personasList;
-    }
-
-    @Override
-    public String toString() {
-        return "SeguridadSocial{" +
-                "personasList=" + personasList +
-                '}';
+        return new ArrayList<>(personaMapDNI.values());
     }
 }
